c261b18ae5a5596faf291052fd5f56dc932b0eb5
ericmuh <muhwezi1000@gmail.com>
2020-09-28 12:50:58 +0300

    simple react app using cdn links

---
+++ app.js
@@ -0,0 +1,9 @@
+
+      // function which return some JSX
+      function HelloWorld() {
+        return <h3>Hello World from React</h3>; //JSX
+      }
+
+      ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
+
+      //Comes from react-dom allows to connect our app to the DOM
