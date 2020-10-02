9d2800fa3f776dfb79415658e76327849e80ece1
ericmuh <muhwezi1000@gmail.com>
2020-10-02 14:36:26 +0300

    remove commenrs app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from "react"
 import ReactDOM from 'react-dom'
 function HelloWorld() {
-  return <h3>Hello World from React</h3>; //JSX
+  return <h3>Hello World from React</h3>; 
 }
 
 console.log("something");
 ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
 
-//Comes from react-dom allows to connect our app to the DOM
 
