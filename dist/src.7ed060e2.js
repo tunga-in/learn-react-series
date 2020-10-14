516018eb8f826462057f6cbc326e8f413e8b4688
ericmuh <muhwezi1000@gmail.com>
2020-10-14 13:55:52 +0300

    update gitignore

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -32449,7 +32449,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34795" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37715" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
