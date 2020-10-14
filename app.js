df9950c354e718644b742aadeb9f065fa7006f85
ericmuh <muhwezi1000@gmail.com>
2020-10-14 15:18:13 +0300

    react-with cdn links

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+// function which return some JSX
+function HelloWorld() {
+  return <h3>Hello World from React</h3>; //JSX
+}
 
-      // function which return some JSX
-      function HelloWorld() {
-        return <h3>Hello World from React</h3>; //JSX
-      }
+ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
 
-      ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
-
-      //Comes from react-dom allows to connect our app to the DOM
+//Comes from react-dom allows to connect our app to the DOM
