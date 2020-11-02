1baf535242c83b892a6890542299e5085add2e60
ericmuh <muhwezi1000@gmail.com>
2020-11-02 18:26:56 +0300

    change class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="footer bg-dark">
-      <div class="container">
-        <span class="text-muted">footer Content.</span>
+    <footer className="footer bg-dark">
+      <div className="container">
+        <span className="text-muted">footer Content.</span>
       </div>
     </footer>
   );
